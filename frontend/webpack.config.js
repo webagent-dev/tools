0c2d38ff859c6703a6f1d51dc1512346fc12844c
webagent-dev <webagent24@gmail.com>
2022-07-21 05:46:11 +0100

    complete webpack and babel config files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,15 +3,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: path.join(__dirname, "src", "index.js"),
     output: {
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        assetModuleFilename: "public/[hash][ext][query]"
+    },
+    target: "web",
+    devServer: {
+        port: 3000,
+        static: ["./public"],
+        open: true,
+        hot: true,
+        liveReload: true
+    },
+    resolve: {
+        extensions: [ ".js", ".jsx", "json"]
     },
     module: {
         rules: [
-            {
+            {   
                 test: /\.?(js|jsx)$/,
                 exclude: /node_module/,
                 use: {
                     loader: "babel-loader"
+                },
+                options: {
+                   presets: ["@babel/preset-env", "@babel/preset-react"] 
                 }
             },
         ]
