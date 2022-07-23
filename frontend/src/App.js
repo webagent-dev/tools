ddca0bfc0e55b7d3d98234ede177208c975a15e6
webagent-dev <webagent24@gmail.com>
2022-07-24 00:39:30 +0100

    add asset files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { About, Contact, Home, Shortner, Tech,  Img, Youtube } from './files'
+import { About, Contact, Home, Shortner, Tech,  Img, Youtube, Nav } from './files'
 import { Routes, Route } from 'react-router-dom'
 import { AppContainer, GlobalStyle } from './app.style'
 function App() {
   return (
     <AppContainer>
       <GlobalStyle />
-      App
+      <Nav />
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
     </AppContainer>
   )
 }
--- frontend/src/files.js
+++ frontend/src/files.js
@@ -5,7 +5,9 @@ import { default as Shortner } from './pages/Shortner'
 import { default as Tech } from './pages/Tech'
 import { default as Img } from './pages/Img'
 import { default as Youtube } from './pages/Youtube'
+import { default as Nav } from './comps/nav/Nav'
+import { default as Home } from './comps/home/Home'
 
 
 
-export { About, Contact, Home, Shortner, Tech, Img, Youtube }
+export { About, Contact, Home, Shortner, Tech, Img, Youtube, Home, Nav } 
