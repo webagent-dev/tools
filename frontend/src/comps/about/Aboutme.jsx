3c2d28479eb58751d3c97662a9e87b63d444b7b3
webagent-dev <webagent24@gmail.com>
2022-07-26 07:15:09 +0100

    keep working on about me

--- frontend/src/comps/about/Aboutme.jsx
+++ frontend/src/comps/about/Aboutme.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
-import { AboutmeContainer } from './about.style'
+import { AboutmeContainer, Divider, Stack, Header, Details, Img, Text, TechDetail, Icon, Name, Tech } from './about.style'
 function Aboutme() {
     return (
         <AboutmeContainer>
-            Aboutme
+            <Divider>
+                <Header></Header>
+                <Details>
+                    <Img />
+                    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis temporibus cupiditate omnis? Fugit ab doloremque, modi dolor suscipit aperiam quo asperiores illo, doloribus nobis quod ipsum reiciendis magnam, error dolorum corporis maiores repellat atque eius tempore alias officiis quia quaerat cum! Aliquid facere ad voluptas accusamus laudantium ab hic provident?</Text>
+                </Details>
+            </Divider>
+            <Divider>
+                <Header></Header>
+                <Tech>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                    <TectDetail>
+                        <Icon />
+                        <Name>Lorem, ipsum.</Name>
+                    </TectDetail>
+                </Tech>
+            </Divider>
         </AboutmeContainer>
     )
 }
--- frontend/src/comps/about/about.style.js
+++ frontend/src/comps/about/about.style.js
@@ -3,5 +3,21 @@ import styled from 'styled-components'
 export const AboutmeContainer = styled.div`
     margin-top: 40px;
     flex: 4;
-    background: red;
+
 `
+
+export const Divider = styled.div`
+ width: 100%;
+ height: auto;
+ background: whitesmoke;
+`
+// export const Stack = styled.div``
+export const Stack = styled.div``
+export const Header = styled.div``
+export const Details = styled.div``
+export const Img = styled.div``
+export const Text = styled.div``
+export const TechDetail = styled.div``
+export const Icon = styled.div``
+export const Name = styled.div``
+export const Tech = styled.div``
